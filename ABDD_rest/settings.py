b62dee746ca3b221fb576252103be43b22843bd3
davidkingg <davidoluseje@gmail.com>
2023-05-19 01:08:58 +0100

    static root

--- ABDD_rest/settings.py
+++ ABDD_rest/settings.py
@@ -156,13 +156,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR/'static'
-]
-#STATIC_ROOT = os.path.join(BASE_DIR, 'sub_app','static')
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR/'static'
+# ]
+# #STATIC_ROOT = os.path.join(BASE_DIR, 'sub_app','static')
+
+# # Default primary key field type
+# # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'build/static')
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
