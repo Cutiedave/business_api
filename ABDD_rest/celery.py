9cb1e851e7c69b5d32edf48960512c66f3dfaeab
davidkingg <davidoluseje@gmail.com>
2023-05-30 18:04:41 +0100

    pro71

--- ABDD_rest/celery.py
+++ ABDD_rest/celery.py
@@ -3,7 +3,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ABDD_rest.settings")
-#app = Celery("ABDD_rest", broker_url="redis://default:bLVoXmvY0aulFkAc5i30@containers-us-west-35.railway.app:7756/1")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ABDD_rest.settings")
+app = Celery("ABDD_rest", broker_url="redis://default:bLVoXmvY0aulFkAc5i30@containers-us-west-35.railway.app:7756/1")
 #app.config_from_object("django.conf:settings")
-#app.autodiscover_tasks()
+app.autodiscover_tasks()
