28ac9954093c2c0f70d38d3faa8c2de3fbbf319a
davidkingg <davidoluseje@gmail.com>
2023-05-30 15:00:59 +0100

    proc2

--- ABDD_rest/celery.py
+++ ABDD_rest/celery.py
@@ -4,6 +4,6 @@ import os
 from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ABDD_rest.settings")
-app = Celery("django_celery")
+app = Celery("ABDD_rest")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
