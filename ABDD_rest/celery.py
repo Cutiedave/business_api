7de3fabd6a83631808377ef338302e1d635e2e09
davidkingg <davidoluseje@gmail.com>
2023-05-30 19:07:02 +0100

    celery fix2

--- ABDD_rest/celery.py
+++ ABDD_rest/celery.py
@@ -9,7 +9,9 @@ app = Celery("ABDD_rest", broker_url="redis://default:bLVoXmvY0aulFkAc5i30@conta
 #app.config_from_object("django.conf:settings")
 app.conf.worker_max_tasks_per_child = 100
 
-celery_max_mem_kilobytes = (psutil.virtual_memory().total * 0.5) / 1024
+celery_max_mem_kilobytes = (psutil.virtual_memory().total * 0.4) / 1024
+
+app.conf.worker_concurrency=1
 
 app.conf.worker_max_memory_per_child = int(celery_max_mem_kilobytes / app.conf.worker_concurrency)
 
