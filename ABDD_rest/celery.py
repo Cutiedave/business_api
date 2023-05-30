1c701569b180bb579f6ea85dbcf527ee889be6fd
davidkingg <davidoluseje@gmail.com>
2023-05-30 18:42:30 +0100

    celery fix

--- ABDD_rest/celery.py
+++ ABDD_rest/celery.py
@@ -1,14 +1,16 @@
 # django_celery/celery.py
 
 import os
+import psutil
 from celery import Celery
 
 #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ABDD_rest.settings")
 app = Celery("ABDD_rest", broker_url="redis://default:bLVoXmvY0aulFkAc5i30@containers-us-west-35.railway.app:7756/1")
 #app.config_from_object("django.conf:settings")
-app.conf.update(
-     CELERY_ACCEPT_CONTENT = ['json'],
-     CELERY_TASK_SERIALIZER = 'json',
-     CELERY_RESULT_SERIALIZER = 'json', 
-)
+app.conf.worker_max_tasks_per_child = 100
+
+celery_max_mem_kilobytes = (psutil.virtual_memory().total * 0.5) / 1024
+
+app.conf.worker_max_memory_per_child = int(celery_max_mem_kilobytes / app.conf.worker_concurrency)
+
 app.autodiscover_tasks()
